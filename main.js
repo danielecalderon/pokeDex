4c1d833f3919b4bf4a2fa2b6b94f6fe3b58669ac
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2022-01-10 20:36:01 -0500

    Adding bootstrap

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+<script type="module"/>
+  import { Toast } from 'bootstrap.esm.min.js'
+
+
 
 function fetchAllPokemon(){
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
@@ -26,6 +30,15 @@ function renderPokemon(pokeData){
     let pokeNumber = document.createElement('p')
     pokeNumber.innerText = `#${pokeData.id}`
     let pokeTypes = document.createElement('ul') 
+
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     
     createTypes(pokeData.types, pokeTypes) 
     
